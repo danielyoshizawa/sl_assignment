28927a4f68e4a93747101b51aa9c2ea90e8d750f
danielyoshizawa <yoshidanielcwb@gmail.com>
2018-09-25 01:44:42 -0300

    Initial Server implementation

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,7 +1,62 @@
 #include <iostream>
+#include <Windows.h>
 
-int main()
+int main(int argc, const char **argv)
 {
-	std::cout << "Hello StreamLab at Vancouver" << std::endl;
+	std::cout << "Starting Named Pipe Server" << std::endl;
+
+	HANDLE pipe = CreateNamedPipe(
+		"\\\\.\\pipe\\my_pipe",
+		PIPE_ACCESS_OUTBOUND,
+		PIPE_TYPE_BYTE,
+		1,
+		0,
+		0,
+		0,
+		NULL
+	);
+
+	if (pipe == NULL || pipe == INVALID_HANDLE_VALUE)
+	{
+		std::cout << "Failed to create pipe :: " << GetLastError() << std::endl;
+		system("pause");
+		return 1;
+	}
+
+	std::cout << "Waiting client" << std::endl;
+
+	BOOL result = ConnectNamedPipe(pipe, NULL);
+	if (!result)
+	{
+		std::cout << "Failed to connect :: " << GetLastError() << std::endl;
+		CloseHandle(pipe);
+		system("pause");
+		return 1;
+	}
+
+	std::cout << "Sending data" << std::endl;
+	const wchar_t *data = L"*** Hello pipe ***";
+	DWORD numBytesWritten = 0;
+	result = WriteFile(
+		pipe,
+		data,
+		wcslen(data),
+		&numBytesWritten,
+		NULL
+	);
+
+	if (result)
+	{
+		std::cout << "Number of bytes sent : " << numBytesWritten << std::endl;
+	}
+	else
+	{
+		std::cout << "Failed to send data :: " << GetLastError() << std::endl;
+	}
+
+	CloseHandle(pipe);
+
+	std::cout << "Exiting..." << std::endl;
+	system("pause");
 	return 0;
 }
